32e9ba17b6e4b1c3170388b46c4f559fb2f04717
Teja Sureddy <tejasteyn@gmail.com>
2024-04-05 10:54:23 +0530

    reportlab full pdf generation progress

--- my_apps/pdf/views/pdf_generation.py
+++ my_apps/pdf/views/pdf_generation.py
@@ -1,12 +1,8 @@
 from django.http import HttpResponse
 from reportlab.lib import colors
-from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Image, Spacer
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.lib.units import inch
-from reportlab.lib.utils import ImageReader
-from django.contrib.staticfiles.finders import find
-from my_project.settings import BASE_DIR
+from reportlab.lib.pagesizes import A4
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Spacer
+from my_apps.pdf.report_labs import CustomReportLabMethods
 
 
 def generate_pdf_view(request):
@@ -14,53 +10,98 @@ def generate_pdf_view(request):
     It uses reportlab to generate the pdf and send it as a response.
     """
     if request.method == 'GET':
+        # disposition: 'inline' will render the pdf, 'attachment' will download the pdf
+        disposition = 'attachment' if request.GET.get('download') else 'inline'
         response = HttpResponse(content_type='application/pdf')
-        response['Content-Disposition'] = 'attachment; filename="generated_pdf.pdf"'
-        pdf = SimpleDocTemplate(response, pagesize=letter)
+        response['Content-Disposition'] = f'{disposition}; filename="generated_pdf.pdf"'
+        pdf = SimpleDocTemplate(response, pagesize=A4, leftMargin=50, rightMargin=50, topMargin=50, bottomMargin=50)
 
-        # Add a logo to the PDF
-        logo_path = find(BASE_DIR.joinpath('static/assets/google.png'))
-        logo = ImageReader(logo_path)
-        logo_width, logo_height = logo.getSize()
-        aspect_ratio = logo_height / logo_width
-        logo = logo_path and Image(logo_path, width=1 * inch, height=(1 * inch) * aspect_ratio) or None
-
-        # Create a Paragraph for text
-        styles = getSampleStyleSheet()
-        text = """Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
-        industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled 
-        it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic 
-        typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset 
-        sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker 
-        including versions of Lorem Ipsum."""
-        paragraph = Paragraph(text, styles["Normal"])
-
-        # Create a table with data
-        data = [
-            ['Column 1', 'Column 2', 'Column 3'],
-            ['Data 1', 'Data 2', 'Data 3'],
-            ['Data 4', 'Data 5', 'Data 6']
-        ]
-        col_widths = [(letter[0] - 2 * inch) / len(data[0])] * len(data[0])
-        table = Table(data, colWidths=col_widths)
-        style = TableStyle([
-            ('BACKGROUND', (0, 0), (-1, 0), colors.lightblue),
-            ('TEXTCOLOR', (0, 0), (-1, 0), colors.black),
-            ('ALIGN', (0, 0), (-1, -1), 'LEFT'),
-            ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-            ('BACKGROUND', (0, 1), (-1, -1), colors.white),
-            ('GRID', (0, 0), (-1, -1), 0.5, colors.black)])
-        table.setStyle(style)
-
-        # Build elements
-        elements = []
-        if logo:
-            elements.append(logo)
-            elements.append(Spacer(1, 0.2 * inch))
-        elements.append(paragraph)
-        elements.append(Spacer(1, 0.2 * inch))
-        elements.append(table)
-
-        # Add elements to the PDF
-        pdf.build(elements)
+        instance = ReportLabPdfElements()
+        header_content = instance.get_pdf_content()
+        pdf.build(header_content)
         return response
+
+
+class ReportLabPdfElements(CustomReportLabMethods):
+
+    def get_pdf_content(self):
+        header = self.get_header()
+        body = self.get_body()
+        return [header, Spacer(0, 30)] + body
+
+    def get_header(self):
+        # logo
+        logo = self.get_logo(path='assets/logo.png', width=0.5, height=0.5)
+
+        # address
+        address_content1 = '<b>Django Project</b>'
+        address_content2 = '6162 Honey Bluff Parkway<br/>Calder, Michigan, 34567-8912<br/>United States'
+        address = self.get_title_p(address_content1, address_content2)
+
+        # table
+        table = Table([[logo, address]], colWidths=[320, 180])
+        styles = self.get_table_styles()
+        table.setStyle(TableStyle(styles))
+        return table
+
+    def get_body(self):
+        # table1
+        table1_left_content1 = "Invoice #000000006"
+        table1_left = self.get_p(table1_left_content1, style='Heading1', textColor='#ffffff')
+
+        table1_right = Table([
+            ['Order', self.get_p('#110000000006', textColor='#ffffff', alignment=2, is_bold=True)],
+            ['Date', self.get_p('Nov 01, 2024', textColor='#ffffff', alignment=2, is_bold=True)],
+            ['Time', self.get_p('12:50 PM', textColor='#ffffff', alignment=2, is_bold=True)]
+        ])
+        table1_right_styles = self.get_table_styles(text_color='#ffffff', bg_color='#727cf5', padding=0)
+        table1_right.setStyle(TableStyle(table1_right_styles))
+
+        table1 = Table([[table1_left, table1_right]], colWidths=[320, 180])
+        table1_styles = self.get_table_styles(bg_color='#000865')
+        table1_styles += [('BACKGROUND', (-1, -1), (-1, -1), colors.HexColor('#727cf5'))]
+        table1.setStyle(TableStyle(table1_styles))
+
+        # table2
+        table2_left_content = '''Veronica Costello<br/>6162 Honey Bluff Parkway<br/>Calder, Michigan, 
+                                 34567-8912<br/>United States<br/>T: (555) 321-4567'''
+        table2_left = self.get_p(table2_left_content)
+
+        table2_right = [self.get_title_p('Shipping Method', 'Flat Rate - Fixed'), Spacer(0, 15),
+                        self.get_title_p('Payment Method', 'Check / Money order')]
+
+        table2 = Table([[table2_left, table2_right]], colWidths=[320, 180])
+        table2_styles = self.get_table_styles(padding_y=0, line_before_weight=1, line_before_color='#f0f1f3')
+        table2.setStyle(TableStyle(table2_styles))
+
+        # table3
+        table3_content = [
+            ['Items', 'Qty', 'Subtotal'],
+            [self.get_title_p('Clamber Watch', 'SKU: 24-WG03', False), self.get_p('2'),
+             self.get_p('$100.00', alignment=2)],
+            [self.get_title_p('Olivia 1/4 Zip Light Jacket', 'SKU: WJ12-M-Purple', False), self.get_p('1'),
+             self.get_p('$77.00', alignment=2)],
+            [self.get_title_p('Rival Field Messenger', 'SKU: 24-MB06', False), self.get_p('1'),
+             self.get_p('$145.00', alignment=2)]
+        ]
+        table3 = Table(table3_content, colWidths=[250, 125, 125])
+        table3_styles = self.get_table_styles(text_color='#ffffff', rowBg=None, padding_x=4, padding_y=6)
+        table3_styles += [('BACKGROUND', (0, 0), (-1, 0), colors.HexColor('#727cf5')),
+                          ('ALIGN', (-1, 0), (-1, 0), 'RIGHT')]
+        table3.setStyle(TableStyle(table3_styles))
+
+        # table4
+        table4_content = [
+            ['', self.get_p('Subtotal', alignment=2), self.get_p('$222.00', alignment=2)],
+            ['', self.get_p('Discount', alignment=2), self.get_p('-$24.40', alignment=2)],
+            ['', self.get_p('Tax', alignment=2), self.get_p('$36.06', alignment=2)],
+            ['', self.get_p('Shipping & Handling', alignment=2), self.get_p('$20.00', alignment=2)],
+            ['', self.get_p('Grand Total', alignment=2), self.get_p('$256.66', alignment=2)],
+        ]
+        table4 = Table(table4_content, colWidths=[250, 125, 125])
+        table4_styles = self.get_table_styles(align='RIGHT', grid_weight=1, box_weight=1, grid_color='#ffffff',
+                                              box_color='#ffffff', padding_x=4, padding_y=6)
+        table4_styles += [('BACKGROUND', (-1, 0), (-1, -1), colors.HexColor('#f0f1f3'))]
+        table4.setStyle(TableStyle(table4_styles))
+
+        return [table1, Spacer(0, 30), table2, Spacer(0, 30), table3, table4]
