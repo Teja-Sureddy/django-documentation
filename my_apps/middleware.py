de13c1fe009eeb7d8bdf16a166fe76eb7c89dfd2
Teja Sureddy <tejasteyn@gmail.com>
2024-04-14 15:28:38 +0530

    response security headers added

--- my_apps/middleware.py
+++ my_apps/middleware.py
@@ -3,6 +3,7 @@ A pipeline is a series of middlewares that process a request and response in a s
 
 Middleware allows you to alter the processing of requests and responses.
 """
+
 from rest_framework.authentication import TokenAuthentication
 import json
 from my_apps.users.models import AuditLog
@@ -19,7 +20,7 @@ class AuditLogMiddleware:
         if not user:
             try:
                 user, token = TokenAuthentication().authenticate(request)
-            except:
+            except Exception:
                 pass
 
         # audit
@@ -29,7 +30,7 @@ class AuditLogMiddleware:
         params = json.dumps(dict(request.GET))
 
         if self.is_valid_path(path):
-            obj = {'user': user, 'method': method, 'path': path, 'params': params}
+            obj = {"user": user, "method": method, "path": path, "params": params}
             AuditLog.objects.create(**obj)
 
         response = self.get_response(request)
@@ -39,13 +40,47 @@ class AuditLogMiddleware:
     def is_valid_path(path: str):
         is_valid = True
 
-        ignored_names = ['home_redirect', 'login_redirect']
+        ignored_names = ["home_redirect", "login_redirect"]
         for name in ignored_names:
             if path == reverse(name):
                 is_valid = False
                 break
 
-        if '__debug__' in path:
+        if "__debug__" in path:
             is_valid = False
 
         return is_valid
+
+
+class SecurityHeadersMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
+
+        # Informs browsers that the site should only be accessed using HTTPS for a specified period of time.
+        response["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
+
+        # It prevents MIME-sniffing attacks.
+        response["X-Content-Type-Options"] = "nosniff"
+
+        # It prevents a web page from being displayed within an iframe, embed, ...
+        response["X-Frame-Options"] = "DENY"
+
+        # It helps detect and block certain types of XSS attacks.
+        response["X-XSS-Protection"] = "1; mode=block"
+
+        # It controls how much referrer information should be included with requests.
+        response["Referrer-Policy"] = "no-referrer"
+
+        # Web developers can explicitly declare what functionality can and cannot be used on a website.
+        response["Feature-Policy"] = (
+            "camera 'self'; microphone 'self'; fullscreen 'self' 'allowfullscreen'"
+        )
+        response["Permissions-Policy"] = (
+            "camera=(self), microphone=(self), geolocation=(), accelerometer=(), "
+            "gyroscope=(), magnetometer=(), payment=(), fullscreen=(self)"
+        )
+
+        return response
--- my_project/settings.py
+++ my_project/settings.py
@@ -19,16 +19,16 @@ env = environ.Env(DEBUG=(bool, False))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fwbxeelgwxbx*+xp=qk7q07n7d#yaaafqrc1$!l@4lbhs+yyz-'
+SECRET_KEY = "django-insecure-fwbxeelgwxbx*+xp=qk7q07n7d#yaaafqrc1$!l@4lbhs+yyz-"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool('DEBUG', False)
+DEBUG = env.bool("DEBUG", False)
 
 ALLOWED_HOSTS = []
 
@@ -37,81 +37,83 @@ INTERNAL_IPS = ["127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # 3rd party
-    'debug_toolbar',
-    'django_tables2',
-    'django_filters',
-    'django_htmx',
-    'phonenumber_field',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
-    'allauth.mfa',
-    'rest_framework',
-    'rest_framework.authtoken',
-    'notifications',
-    'channels',
-    'django_q',
+    "debug_toolbar",
+    "django_tables2",
+    "django_filters",
+    "django_htmx",
+    "phonenumber_field",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.google",
+    "allauth.mfa",
+    "rest_framework",
+    "rest_framework.authtoken",
+    "notifications",
+    "channels",
+    "django_q",
     # custom
-    'my_apps.dashboard',
-    'my_apps.users',
-    'my_apps.rest',
-    'my_apps.notification',
-    'my_apps.pdf',
+    "my_apps.dashboard",
+    "my_apps.users",
+    "my_apps.rest",
+    "my_apps.notification",
+    "my_apps.pdf",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'django_htmx.middleware.HtmxMiddleware',
-    'allauth.account.middleware.AccountMiddleware',
-    'my_apps.middleware.AuditLogMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "django_htmx.middleware.HtmxMiddleware",
+    "allauth.account.middleware.AccountMiddleware",
+    "my_apps.middleware.AuditLogMiddleware",
+    "csp.middleware.CSPMiddleware",
+    "my_apps.middleware.SecurityHeadersMiddleware",
 ]
 
-ROOT_URLCONF = 'my_project.urls'
+ROOT_URLCONF = "my_project.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [str(BASE_DIR / "templates")],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [str(BASE_DIR / "templates")],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'my_project.wsgi.application'
+WSGI_APPLICATION = "my_project.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'my_db',
-        'USER': env('DB_USER'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': env('DB_HOST'),
-        'PORT': '5432'
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "my_db",
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": "5432",
     }
 }
 
@@ -120,25 +122,25 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -147,84 +149,83 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Media files (files, images)
-MEDIA_URL = 'assets/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
+MEDIA_URL = "assets/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "assets")
 
 # Custom
 # auth
 AUTH_USER_MODEL = "users.User"
 
 # phone number field
-PHONENUMBER_DEFAULT_REGION = 'IN'
+PHONENUMBER_DEFAULT_REGION = "IN"
 
 # all-auth
 AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
+    "django.contrib.auth.backends.ModelBackend",
     # 'allauth.account.auth_backends.AuthenticationBackend',
-    'my_apps.backend.MyCustomAuthBackend'
+    "my_apps.backend.MyCustomAuthBackend",
 ]
-LOGIN_URL = '/user/login/'
-LOGIN_REDIRECT_URL = '/dashboard/data/'
+LOGIN_URL = "/user/login/"
+LOGIN_REDIRECT_URL = "/dashboard/data/"
 ACCOUNT_FORMS = {
-    'signup': 'my_apps.users.forms.CustomSignupForm',
-    'login': 'my_apps.users.forms.CustomLoginForm',
-    'reset_password': 'my_apps.users.forms.CustomPasswordResetForm',
-    'reset_password_from_key': 'my_apps.users.forms.CustomPasswordResetKeyForm',
-    'reauthenticate': 'my_apps.users.forms.CustomReauthenticateForm',
+    "signup": "my_apps.users.forms.CustomSignupForm",
+    "login": "my_apps.users.forms.CustomLoginForm",
+    "reset_password": "my_apps.users.forms.CustomPasswordResetForm",
+    "reset_password_from_key": "my_apps.users.forms.CustomPasswordResetKeyForm",
+    "reauthenticate": "my_apps.users.forms.CustomReauthenticateForm",
 }
 MFA_FORMS = {
-    'activate_totp': 'my_apps.users.forms.CustomActivateTOTPForm',
-    'authenticate': 'my_apps.users.forms.CustomAuthenticateForm',
-    'reauthenticate': 'my_apps.users.forms.CustomAuthenticateForm',
+    "activate_totp": "my_apps.users.forms.CustomActivateTOTPForm",
+    "authenticate": "my_apps.users.forms.CustomAuthenticateForm",
+    "reauthenticate": "my_apps.users.forms.CustomAuthenticateForm",
 }
-SOCIALACCOUNT_ADAPTER = 'my_apps.users.utils.CustomSocialAccountAdapter'
+SOCIALACCOUNT_ADAPTER = "my_apps.users.utils.CustomSocialAccountAdapter"
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_USER_MODEL_USERNAME_FIELD = None
-ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 # ACCOUNT_EMAIL_NOTIFICATIONS = True
 SOCIALACCOUNT_EMAIL_AUTHENTICATION_AUTO_CONNECT = True
 SOCIALACCOUNT_EMAIL_AUTHENTICATION = True
 SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        'SCOPE': ['profile', 'email'],
-        'AUTH_PARAMS': {'access_type': 'online'},
-        'APP': {
-            'client_id': env('GOOGLE_OAUTH_CLIENT'),
-            'secret': env('GOOGLE_OAUTH_SECRET'),
-            'key': ''
-        }
+    "google": {
+        "SCOPE": ["profile", "email"],
+        "AUTH_PARAMS": {"access_type": "online"},
+        "APP": {
+            "client_id": env("GOOGLE_OAUTH_CLIENT"),
+            "secret": env("GOOGLE_OAUTH_SECRET"),
+            "key": "",
+        },
     }
 }
 MFA_ADAPTER = "allauth.mfa.adapter.DefaultMFAAdapter"
 
 # email
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = env('EMAIL_HOST')
-EMAIL_PORT = env.int('EMAIL_PORT')
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env.int("EMAIL_PORT")
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = env('EMAIL_USER')
-EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')
+EMAIL_HOST_USER = env("EMAIL_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_PASSWORD")
 
 # channels
-ASGI_APPLICATION = 'my_project.asgi.application'
+ASGI_APPLICATION = "my_project.asgi.application"
 
 # channels redis
 CHANNEL_LAYERS = {
-    'default': {
+    "default": {
         # without broker - in memory
-        'BACKEND': 'channels.layers.InMemoryChannelLayer',
-
+        "BACKEND": "channels.layers.InMemoryChannelLayer",
         # with redis broker
         # 'BACKEND': 'channels_redis.core.RedisChannelLayer',
         # 'CONFIG': {
@@ -235,54 +236,54 @@ CHANNEL_LAYERS = {
 
 # django-q
 Q_CLUSTER = {
-    'name': 'DjangoORM',
-    'workers': 4,  # Number of simultaneous tasks
-    'timeout': 90,
-    'retry': 90,
-    'queue_limit': 100,
-    'bulk': 10,
-    'orm': 'default',
+    "name": "DjangoORM",
+    "workers": 4,  # Number of simultaneous tasks
+    "timeout": 90,
+    "retry": 90,
+    "queue_limit": 100,
+    "bulk": 10,
+    "orm": "default",
 }
 
 # logging - inbuilt
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple',
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
         },
-        'file': {
-            'level': 'INFO',
+        "file": {
+            "level": "INFO",
             # use RotatingFileHandler or TimedRotatingFileHandler to get logs in multiple files.
-            'class': 'logging.FileHandler',
-            'filename': './logs/django.log',
-            'formatter': 'simple'
+            "class": "logging.FileHandler",
+            "filename": "./logs/django.log",
+            "formatter": "simple",
         },
     },
-    'formatters': {
-        'simple': {
-            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    "formatters": {
+        "simple": {
+            "format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
         },
     },
     "root": {
-        "handlers": ['console', 'file'],
+        "handlers": ["console", "file"],
         "level": "INFO",
     },
-    'loggers': {
-        'django': {
-            'handlers': ['console', 'file'],
-            'level': 'INFO',
-            'propagate': True,
+    "loggers": {
+        "django": {
+            "handlers": ["console", "file"],
+            "level": "INFO",
+            "propagate": True,
         },
     },
 }
 
 # cache - inbuilt
 CACHES = {
-    'default': {
+    "default": {
         # without redis - in memory
         # "BACKEND": "django.core.cache.backends.db.DatabaseCache", # db caching
         # 'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
@@ -290,9 +291,19 @@ CACHES = {
         # 'OPTIONS': {
         #     'MAX_ENTRIES': 1000
         # },
-
         # with redis
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379",
     }
 }
+
+# csp
+CSP_DEFAULT_SRC = (
+    "'self'",
+    "'unsafe-inline'",
+    "cdnjs.cloudflare.com",
+    "ajax.googleapis.com",
+    "cdn.jsdelivr.net",
+    "fonts.googleapis.com",
+    "fonts.gstatic.com",
+)
